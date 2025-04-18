f5ed1f1d8cd3db9be2f300a24b126036b78d9be4
Leon Lonsdale <leon.codes@icloud.com>
2025-04-18 23:03:31 +0100

    feat(exceptions): add custom 'bank' exception classes for error handling

---
+++ utils/exceptions/bank.py
@@ -0,0 +1,18 @@
+class BankError(Exception):
+    """Base exception for bank-related operations."""
+
+
+class BankAccountNotFoundError(BankError):
+    pass
+
+
+class BankAccountHasBalanceError(BankError):
+    pass
+
+
+class BankAccountDeletionError(BankError):
+    pass
+
+
+class BankAccountValidationError(BankError):
+    pass
