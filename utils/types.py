1e07044341ecbf5dce208c7a1016865b4c2cd54d
Leon Lonsdale <leon.codes@icloud.com>
2025-04-18 10:37:02 +0100

    feat(utils): add 'TableName' Enum for type-safe DB table names

---
+++ utils/types.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class TableName(Enum):
+    BANKS = "banks"
+    CREDITCARDS = "credit_cards"
+    LOANS = "loans"
+    BILLS = "bills"
+    SUBSCRIPTIONS = "subscriptions"
+    TRANSACTIONS = "transactions"
