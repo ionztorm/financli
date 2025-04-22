3c42a24f70927daf660e826cbbb0a9a60037d90a
Leon Lonsdale <leon.codes@icloud.com>
2025-04-22 10:40:16 +0100

    fix(script): make test runner handle spaces and be POSIX-compatible

--- run_tests.sh
+++ run_tests.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+
 PROJECT_DIR=$(dirname "$(realpath "$0")")
+cd "$PROJECT_DIR" || exit 1
 
-test_files=$(find "$PROJECT_DIR" -type f -name 'test_*.py')
+test_files=()
 
-if [ -z "$test_files" ]; then
+while IFS= read -r file; do
+    relative_path="${file#./}"
+    test_files+=("$relative_path")
+done < <(find . -type f -name 'test_*.py')
+
+if [ ${#test_files[@]} -eq 0 ]; then
     echo "No test files found!"
     exit 1
 fi
 
 echo "Running all found tests..."
-python3 -m unittest $(echo "$test_files") -v
 
-echo "\nAll tests execution finished."
-
+python3 -m unittest -v "${test_files[@]}"
 test_exit_code=$?
+
+echo -e "\nAll tests execution finished."
+
 if [ $test_exit_code -ne 0 ]; then
-    echo "\nSome tests failed. Exiting with status $test_exit_code"
+    echo -e "\nSome tests failed. Exiting with status $test_exit_code"
 fi
 
 exit $test_exit_code
