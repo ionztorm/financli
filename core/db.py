8cc5bfe1729378b996437435d40aab25cf92efe8
Leon Lonsdale <leon.codes@icloud.com>
2025-04-28 10:16:03 +0100

    chore(db): update database initialisation to include new schemas for StoreCards, CreditCards, Bills, and Subscriptions

--- core/db.py
+++ core/db.py
@@ -2,7 +2,11 @@ import sqlite3
 
 from pathlib import Path
 
+from features.payable.bill.schema import CREATE_BILLS_TABLE
 from features.accounts.bank.schema import CREATE_BANKS_TABLE
+from features.accounts.store_card.schema import CREATE_STORE_CARDS_TABLE
+from features.accounts.credit_card.schema import CREATE_CREDIT_CARDS_TABLE
+from features.payable.subscription.schema import CREATE_SUBSCRIPTIONS_TABLE
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR / "data"
@@ -21,6 +25,10 @@ def get_connection() -> sqlite3.Connection:
     cursor = conn.cursor()
 
     cursor.execute(CREATE_BANKS_TABLE)
+    cursor.execute(CREATE_CREDIT_CARDS_TABLE)
+    cursor.execute(CREATE_BILLS_TABLE)
+    cursor.execute(CREATE_STORE_CARDS_TABLE)
+    cursor.execute(CREATE_SUBSCRIPTIONS_TABLE)
     # TODO: Add remaining tables
 
     conn.commit()
