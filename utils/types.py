54aa13d297a954560a3a97d0df2ae6d5f1b6d1c1
Leon Lonsdale <leon.codes@icloud.com>
2025-04-28 14:28:51 +0100

    feat(types): add enums for IDKeys, AccountTypeKeys, and AccountRole to improve data handling and validation

--- utils/types.py
+++ utils/types.py
@@ -16,3 +16,21 @@ class TransactionType(Enum):
     DEPOSIT = "deposit"
     PAY_ONLY = "pay_only"
     TRANSFER = "transfer"
+
+
+class IDKeys(Enum):
+    ID = "id"
+    SOURCE_ID = "source_id"
+    DESTINATION_ID = "destination_id"
+
+
+class AccountTypeKeys(Enum):
+    DEFAULT = "account_type"
+    SOURCE_TYPE = "source_type"
+    DESTINATION_TYPE = "destination_type"
+
+
+class AccountRole(Enum):
+    DEFAULT = "default"
+    SOURCE = "source"
+    DESTINATION = "destination"
