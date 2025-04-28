fd503a68458c02905ff2becbf43063a5ddb3eda6
Leon Lonsdale <leon.codes@icloud.com>
2025-04-28 10:20:31 +0100

    feat(service): integrate transaction logging into TransactionService and handle logging errors

--- core/transaction_service.py
+++ core/transaction_service.py
@@ -1,12 +1,16 @@
 import sqlite3
 
+from utils.helpers import wrap_error
 from core.utility_service import UtilityService
 from features.payable.base import PayOnly
+from features.transactions.model import Transaction
+from features.transactions.exceptions import TransactionLogError
 
 
 class TransactionService:
     def __init__(self, db_connection: sqlite3.Connection) -> None:
         self.utility = UtilityService(db_connection)
+        self.transactions = Transaction(db_connection)
 
     def deposit(self, data: dict) -> None:
         account_type, account_id = self.utility._get_account_type_and_id(data)
@@ -26,5 +30,10 @@ class TransactionService:
             raise ValueError(f"Cannot withdraw from {account_type}.")
         model.withdraw(account_id, amount)
 
-    def log_transaction(self) -> None:
-        raise NotImplementedError
+    def log_transaction(self, data: dict) -> None:
+        try:
+            self.transactions.log(data)
+        except TransactionLogError as e:
+            raise wrap_error(TransactionLogError, "Transaction logging failed")(
+                e
+            ) from e
