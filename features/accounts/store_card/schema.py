e5d0c3dc6ca67ed6dfd714bdfa37fc3a30608854
Leon Lonsdale <leon.codes@icloud.com>
2025-05-04 22:37:56 +0100

    fix(schema): fix store card schema

--- features/accounts/store_card/schema.py
+++ features/accounts/store_card/schema.py
@@ -1,5 +1,5 @@
 CREATE_STORE_CARDS_TABLE = """
-    CREATE TABLE IF NOT EXISTS banks (
+    CREATE TABLE IF NOT EXISTS store_cards (
         id INTEGER PRIMARY KEY,
         provider TEXT NOT NULL,
         balance REAL NOT NULL,
