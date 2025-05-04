c10205c4b0d37149802556267fb89710d6dcfbd5
Leon Lonsdale <leon.codes@icloud.com>
2025-05-04 22:36:44 +0100

    fix(schema): Credit Card and Store Card schema using incorrect table name

--- features/accounts/credit_card/schema.py
+++ features/accounts/credit_card/schema.py
@@ -1,5 +1,5 @@
 CREATE_CREDIT_CARDS_TABLE = """
-    CREATE TABLE IF NOT EXISTS banks (
+    CREATE TABLE IF NOT EXISTS credit_cards (
         id INTEGER PRIMARY KEY,
         provider TEXT NOT NULL,
         balance REAL NOT NULL,
