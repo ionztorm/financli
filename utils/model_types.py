5320a857c19a59479a201931bbe8baa931ca2ba0
Leon Lonsdale <leon.codes@icloud.com>
2025-05-06 16:06:59 +0100

    feat(types): include Transaction class in ModelType Union

--- utils/model_types.py
+++ utils/model_types.py
@@ -2,5 +2,6 @@ from typing import Union
 
 from features.payable.base import PayOnly
 from features.accounts.base import Accounts
+from features.transactions.model import Transaction
 
-ModelType = Union[Accounts, PayOnly]
+ModelType = Union[Accounts, PayOnly, Transaction]
