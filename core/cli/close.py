b3e5f9e2fbb38bf07e5d06b5174dfe8ba5e2599b
Leon Lonsdale <leon.codes@icloud.com>
2025-05-06 11:21:41 +0100

    feat(cli): add 'close' CLI command

---
+++ core/cli/close.py
@@ -0,0 +1,60 @@
+import argparse
+
+from InquirerPy import inquirer
+from InquirerPy.validator import EmptyInputValidator
+
+from core.db import get_connection
+from utils.helpers import print_table
+from core.controller import Controller
+from core.utils.validator import TYPE_VALIDATORS
+
+ACCOUNT_TYPES = [
+    "bank",
+    "credit card",
+    "store card",
+    "loan",
+    "subscription",
+    "bill",
+]
+
+
+def register_close_command(subparsers: argparse._SubParsersAction) -> None:
+    parser = subparsers.add_parser("close", help="Close an account")
+    parser.add_argument(
+        "--account-type", type=str, help="The type of account to close."
+    )
+    parser.add_argument("--id", type=int, help="ID of the account to close.")
+    parser.set_defaults(func=handle_close)
+
+
+def handle_close(args: argparse.Namespace) -> None:
+    conn = get_connection()
+    model = Controller(conn)
+
+    account_type = args.account_type
+    if not account_type:
+        account_type = inquirer.select(
+            message="Choose account type:",
+            choices=list(ACCOUNT_TYPES),
+        ).execute()
+
+    print(f"\nCurrent {account_type} accounts:")
+
+    accounts = model.list({"account_type": account_type})
+    print_table(accounts)
+
+    validator = TYPE_VALIDATORS.get(int, EmptyInputValidator())
+
+    id = args.id
+    if not id:
+        id = inquirer.text(
+            message="Enter the ID of the account to close: ",
+            validate=validator,
+        ).execute()
+
+    model.close(account_type, id)
+    print(
+        "\nThe account was successfully closed. "
+        f"Your remaining {account_type} accounts are: "
+    )
+    print_table(model.list({"account_type": account_type}))
