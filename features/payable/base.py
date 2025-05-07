ebff322bc8f03b782aabf4385db7bbd16f2ddf8a
Leon Lonsdale <leon.codes@icloud.com>
2025-05-07 15:28:24 +0100

    feat(model): add update() method and associated domain errors to Payable class

--- features/payable/base.py
+++ features/payable/base.py
@@ -10,20 +10,25 @@ from core.exceptions import (
 )
 from features.payable.exceptions import (
     PayableOpenError,
+    PayableUpdateError,
     PayableDeletionError,
     PayableNotFoundError,
 )
 
 
 class PayOnly(Table):
-    def __init__(self, connection: sqlite3.Connection, table_name: TableName) -> None:
+    def __init__(
+        self, connection: sqlite3.Connection, table_name: TableName
+    ) -> None:
         super().__init__(connection, table_name)
 
     def open(self, data: dict) -> None:
         try:
             self._create(data)
         except ValidationError as e:
-            wrapper = wrap_error(PayableOpenError, "Could not create new payable")
+            wrapper = wrap_error(
+                PayableOpenError, "Could not create new payable"
+            )
             raise wrapper(e) from e
         except QueryExecutionError as e:
             wrapper = wrap_error(PayableOpenError, "Payable creation failed")
@@ -39,8 +44,25 @@ class PayOnly(Table):
         try:
             self._delete(id)
         except RecordNotFoundError as e:
-            wrapper = wrap_error(PayableNotFoundError, "Unable to find payable for deletion")
+            wrapper = wrap_error(
+                PayableNotFoundError, "Unable to find payable for deletion"
+            )
             raise wrapper(e) from e
         except QueryExecutionError as e:
-            wrapper = wrap_error(PayableDeletionError, "Could not delete payable")
+            wrapper = wrap_error(
+                PayableDeletionError, "Could not delete payable"
+            )
+            raise wrapper(e) from e
+
+    def update(self, id: int, data: dict) -> None:
+        try:
+            self.get_one(id)[0]
+            self._update(id, data)
+        except RecordNotFoundError as e:
+            wrapper = wrap_error(
+                PayableNotFoundError, "Could not find record to update "
+            )
+            raise wrapper(e) from e
+        except QueryExecutionError as e:
+            wrapper = wrap_error(PayableUpdateError, "Could not update record ")
             raise wrapper(e) from e
