eda28e486d02be8c741353179c91c5412c710e74
Leon Lonsdale <leon.codes@icloud.com>
2025-05-09 20:51:24 +0100

    feat(cli): register import and export parsers

--- core/main.py
+++ core/main.py
@@ -5,7 +5,9 @@ from core.cli.list import register_list_command
 from core.cli.open import register_open_command
 from utils.helpers import load_or_create_settings
 from core.cli.close import register_close_command
+from core.cli.export import register_export_command
 from core.cli.update import register_update_command
+from core.cli.imports import register_import_command
 from core.cli.transaction import register_transact_command
 
 
@@ -27,6 +29,8 @@ def main() -> None:
     register_transact_command(subparsers)
     register_update_command(subparsers)
     register_list_command(subparsers)
+    register_export_command(subparsers)
+    register_import_command(subparsers)
 
     args = parser.parse_args()
 
